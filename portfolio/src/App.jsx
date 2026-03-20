1bdf7987283ab8ef8a723ba014a4ea3f92e7d9ff
Gavin <69440753+vijet1@users.noreply.github.com>
2026-03-20 08:55:47 +0000

    Startup

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,22 @@
 import { useState } from 'react'
 import './index.css'
 
+function setTabHeader(iconLink, title){
+  if (iconLink){
+    const favicon = document.querySelector("link[rel='icon']");
+    favicon.href = iconLink;
+  }
+
+  if (title){
+    document.title = title;
+  }
+}
+setTabHeader("https://avatars.githubusercontent.com/u/69440753?v=4", "Cool Things")
+
 function App() {
   return (
     <>
-     
+     <h1>his</h1>
     </>
   )
 }
--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/vijet1/vijet1.github.io.git",
   plugins: [react()],
 })
